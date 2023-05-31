e1f9dbcee1c3ae8a71ac780b0cea16623ba26a1a
Norton Almeida Pontes <nortonalmeida08@gmail.com>
2023-05-30 23:03:44 -0300

    Comitando Code Wars repositorio

---
+++ CodeWars/HighAndLow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string HighAndLow(string numbers)
+    {
+        int max = 0, min = 0;
+        foreach (char c in numbers)
+        {
+            if (!c.Equals(" "))
+            {
+                if (c.)
+
+            }
+      
+        }
+
+        return $"{max} {min}";
+    }
+}
